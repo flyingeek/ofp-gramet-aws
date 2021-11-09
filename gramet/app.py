855ae5d6ef4e8b73eec3c1788196488f82b3fc72
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-11-09 15:07:20 +0100

    added 304 status code in debug output

--- gramet/app.py
+++ gramet/app.py
@@ -105,9 +105,8 @@ def lambda_handler(event, context):
             },
             'statusCode': 304,
         }
+        print("304 Not modified")
     else:
-        if conditional_etag:
-            print("Conditional Etag {cetag} does not match {etag}".format(cetag=conditional_etag, etag='W/"{etag}"'.format(etag=etag)))
         response_dict = fetch_image(url, etag)
 
     # add CORS headers (on amazon lambda this is already set in the API Gateway/CORS)
